7c6e2267376ae895d5b8404718065e628f405758
LEO LIMA <49534235+lglf77@users.noreply.github.com>
2021-04-23 23:54:28 -0300

    first commit

---
+++ app/src/main/java/com/lglf77/autoscrollinglayout_biblioteca_antiga_ajeitar_para_atual/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lglf77.autoscrollinglayout_biblioteca_antiga_ajeitar_para_atual;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+    }
+}
---
+++ autoscrollinglayout/src/main/java/com/lglf77/autoscrollinglayout/OnPositionCalled.java
@@ -0,0 +1,5 @@
+package com.lglf77.autoscrollinglayout;
+
+public interface OnPositionCalled {
+    public void onPositionCalled(int position);
+}
